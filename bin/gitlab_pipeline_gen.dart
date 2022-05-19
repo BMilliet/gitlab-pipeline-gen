68fa71c9863350513f994ba9277b8006573e1afc
BMilliet <brunomilliet89@gmail.com>
2022-05-19 12:05:43 -0300

    init proj

---
+++ bin/gitlab_pipeline_gen.dart
@@ -0,0 +1,5 @@
+import 'package:gitlab_pipeline_gen/gitlab_pipeline_gen.dart' as gitlab_pipeline_gen;
+
+void main(List<String> arguments) {
+  print('Hello world: ${gitlab_pipeline_gen.calculate()}!');
+}
---
+++ test/gitlab_pipeline_gen_test.dart
@@ -0,0 +1,8 @@
+import 'package:gitlab_pipeline_gen/gitlab_pipeline_gen.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
